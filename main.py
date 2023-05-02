dd2aa50a7fa3d8caa8e9831c41f70730c4f3f457
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2023-05-02 22:09:30 +0300

    Make design for main window

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
+import classes
+
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox
+from PyQt6.QtGui import QIcon
 
 
 class App(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(App, self).__init__()
         self.title = 'Title'
         self.left = 50
         self.top = 100
         self.width = 800
         self.height = 600
+
+        department1 = classes.Department("Department1", 10000, 1500, 10)
+
+        self.label1 = QtWidgets.QLabel(self)
+        self.label1.setText(department1.name)
+        self.label1.move(100, 100)
+        self.label1.adjustSize()
+
+        self.button1 = QPushButton(self)
+        self.button1.move(100, 150)
+        self.button1.setText("Button1")
+        self.button1.clicked.connect(self.button1_click)
+
         self.initUI()
-        button1 = QPushButton("Button1")
-        self.setCentralWidget(button1)
+
+    def button1_click(self):
+        # print("click!")
+        #self.label1.setText("Text changed")
+        #self.label1.adjustSize()
+        mark = classes.Marketing
+        mark.name = "first"
+        classes.defMessageBox(mark.name)
+
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -21,14 +45,21 @@ class App(QMainWindow):
         self.show()
 
 
-#if __name__ == '__main__':
- #   app = QApplication(sys.argv)
-  #  ex = App()
-   # sys.exit(app.exec_())
+
+
+# if __name__ == '__main__':
+#   app = QApplication(sys.argv)
+#  ex = App()
+# sys.exit(app.exec_())
 
 app = QApplication(sys.argv)
 
 window = App()
+# window = QMainWindow()
+
+window.setWindowTitle("Title")
+window.setGeometry(100, 100, 500, 500)
+
 window.show()
 
-app.exec()
+sys.exit(app.exec())
